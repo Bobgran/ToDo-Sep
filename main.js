b28a78eacbce50d39f9b9168a176fd965834ea7c
Måns <mannan.lindroos@gmail.com>
2019-09-03 15:02:30 +0200

    klarknapp och så

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ let fakeDataBase = [
 
 ];
 
+let order = true;
+
+function changeOrder(){
+    order = !order;
+        renderFakeData();
+}
+
 renderFakeData();
 
 function _getId(id)
@@ -12,20 +19,68 @@ function _getId(id)
     return document.getElementById(id);
 }
 
+function deleteTask(index){
+    fakeDataBase.splice(index, 1);
+    renderFakeData();
+
+
+}
+
+function completeTask(index){
+    //fakeDataBase[index].task.fontcolor("green");
+    let objectTask = fakeDataBase[index];
+    objectTask.ready = !objectTask.ready;
+    objectTask.task.fontcolor("green");
+
+    renderFakeData();
+}
+
+_getId("orderBtn").addEventListener("click", changeOrder);
+
 function renderFakeData(){
     //skapar html utifrån vår fakedatabas
-    let htmlOutput = fakeDataBase.map(function(taskObject){
-
+    let htmlOutput = fakeDataBase.map(function(taskObject, index){
+        console.log(index);
         
         return `
         <div>
-        <h1>${taskObject.task} </h1>
-        <button>Delete</button>
+        <h1 id= "${index}">${taskObject.task} <sub>${taskObject.ready} </sub></h1>
+        <button id="${index}" ONCLICK ="deleteTask(${index})">Delete</button>
+        <button ONCLICK ="completeTask(${index})">Complete</button>
         </div>
         
         `;
 
     }); //end map
-    _getId("taskList").innerHTML = htmlOutput.join("");
 
+    if(order == true)
+    {
+    _getId("taskList").innerHTML = htmlOutput.join("");
+    }
+    else
+    {_getId("taskList").innerHTML = htmlOutput.reverse().join("");}
+}
+
+//lyssna efter form submit
+
+_getId("taskForm").addEventListener("submit", addTask);
+
+function addTask(event){
+    //Hindra formuläret att skickas till servern
+    event.preventDefault();
+   //hämta input-datan
+    let inputText =  _getId("taskId").value;
+   //skapa ett task-objekt
+   if(inputText.trim() != "")
+   {
+   let taskObjekt = {id: Date.now(), task:inputText, ready:false}
+   //spara i fakeDataBase
+   fakeDataBase.push(taskObjekt);
+   //rendera på nytt
+   renderFakeData();
+   _getId("taskId").value = "";
+   _getId("taskId").focus();
+   }
+   
+   
 }
